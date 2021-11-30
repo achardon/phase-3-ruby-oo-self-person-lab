1e28dd76377fe14b75feb38b257815af082822a6
Alessandra Chardon <achardon@ucla.edu>
2021-11-30 16:03:39 -0500

    Completed initialization and attributes

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
-# your code goes here
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :hygiene, :happiness
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness
+        @happiness.clamp(0,10)
+    end
+
+    def hygiene
+        @hygiene.clamp(0,10)
+    end
+
+    def happy?
+        happiness > 7
+    end
+
+    def clean?
+        hygiene > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+end
