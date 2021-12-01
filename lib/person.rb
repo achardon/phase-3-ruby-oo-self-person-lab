e2183f3460c78aacbbdfa143601e6d43c1716f6f
Alessandra Chardon <achardon@ucla.edu>
2021-12-01 08:55:03 -0500

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -31,4 +31,35 @@ class Person
         "all about the benjamins"
     end
 
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(friend)
+        friend.happiness += 3
+        self.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        case topic
+        when 'politics'
+            self.happiness -= 2
+            person.happiness -= 2
+            'blah blah partisan blah lobbyist'
+        when 'weather'
+            self.happiness += 1
+            person.happiness += 1
+            'blah blah sun blah rain'
+        else
+            'blah blah blah blah blah'
+        end
+    end
 end
